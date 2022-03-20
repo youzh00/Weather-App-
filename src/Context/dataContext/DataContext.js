c891e2d06129de2b58e70d78e74ff314607cf630
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-03-20 01:07:34 +0100

    night day modes done

--- src/Context/dataContext/DataContext.js
+++ src/Context/dataContext/DataContext.js
@@ -9,20 +9,23 @@ const key = "d724cda68c856a754decbec531b904c5";
 
 export function DataContextProvider({ children }) {
   const key2 = "cc2ee984298825bbf478a71f73670aba";
-  const [weather, setWeather] = useState([]);
+  const [data, setData] = useState([]);
+  const [currentWeather, setCurrentWethaer] = useState();
 
   const getWeather = async (city) => {
     const data = await fetch(
       `http://api.weatherstack.com/current?access_key=${key2}&query=${city}`
     );
     const reponse = await data.json();
-    setWeather(reponse);
+    setData(reponse);
+    setCurrentWethaer(data.current);
   };
   return (
     <DataContext.Provider
       value={{
         getWeather,
-        weather,
+        data,
+        currentWeather,
       }}
     >
       {children}
--- src/Pages/Weather.jsx
+++ src/Pages/Weather.jsx
@@ -4,38 +4,71 @@ import  DataContext  from '../Context/dataContext/DataContext'
 import { TextField } from '@material-ui/core'
 import style from '../Style/Weather.module.css'
 import image from '../assets/weather1.jpg'
+import night from '../assets/night1.jpg'
+import morning1 from '../assets/morning1.jpg'
+import {BsMoonStarsFill} from 'react-icons/bs'
+import {BsFillSunFill} from 'react-icons/bs'
+import {FaCity} from 'react-icons/fa'
+
+
 
 export default function Weather() {
-  const {getWeather,weather}= useContext(DataContext)
+  const {getWeather,data}= useContext(DataContext)
   const [city,setCity]=useState('')
-  
+  const [isDay,setIsDay]=useState('yes')
+  const [submited,setSubmited]=useState(false)
 
+  const {location,current}=data
+  
   const handleChange=(e)=>{
       setCity(e.target.value)
+      setSubmited(false)
   }
   const handleSubmit=(e)=>{
     e.preventDefault()
     getWeather(city)
- 
+    setIsDay(current.is_day)
+    setSubmited(true)
   }
+  console.log(current)
+
+
+
+
+
   return (
     <div className={style.weather}>
-      <img src={image} alt="" className={style.image}/>
+      <img src={isDay==='no' ? night :morning1} alt="" className={style.image}/>
       <div className={style.imageContainer}>
+          <div className={style.dayMode}>
+            {isDay==='no' ? <BsMoonStarsFill size={30}/> : <BsFillSunFill size={30}/>}
+            <p>{isDay==='no' ? 'Night': 'Light '}</p>
+          </div>
+          {submited &&
+           <div className={style.cityContainer}>
+           <FaCity size={25}/>
+           <p>{location.name}</p>
+         </div>}
+         
       </div>
       <div className={style.formContainer}>
       <form onSubmit={handleSubmit} className={style.form}>
         <TextField id="standard-basic" label="City" variant="standard" value={city} onChange={handleChange} className={style.textField} />
       </form> 
+      {current!==undefined &&
       <div className={style.fromContainerDiv}>
-        <p className={style.weatherDetails}>Weather Details</p>
-        <p>Cloud Cover:</p>
-        <p>Humidity:</p>
-        <p>Temperature: C</p>
-        <p>Wind Speed: </p>
-        <p>Visibility: </p>
-        <p>Wind Degree: </p>
-      </div>
+      <p className={style.weatherDetails} > Weather Details :</p>
+      <p className={isDay==='no' ? style.nightPara: style.morningPara}>Cloud Cover: {current.cloudcover} oktas</p>
+      <p className={isDay==='no' ? style.nightPara: style.morningPara}>Humidity: {current.humidity}</p>
+      <p className={isDay==='no' ? style.nightPara: style.morningPara}>Temperature: {current.temperature}&#8451;</p>
+      <p className={isDay==='no' ? style.nightPara: style.morningPara}>Wind Speed: {current.wind_speed} m/s</p>
+      <p className={isDay==='no' ? style.nightPara: style.morningPara}>Visibility: {current.visibility}</p>
+      <p className={isDay==='no' ? style.nightPara: style.morningPara}>Wind Degree: {current.wind_degree}</p>
+      <p className={isDay==='no' ? style.nightPara: style.morningPara}>Weather Description : {current.weather_descriptions[0]}</p>
+    </div>
+     
+     }
+      
       </div>       
     </div>
   )
