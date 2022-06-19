2737b662a1401aa9c57ddfc15c9acca53688d679
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-06-19 21:59:12 +0100

    fexing bugs

--- src/assets/SliderData/SliderData.js
+++ src/assets/SliderData/SliderData.js
@@ -2,9 +2,7 @@ import { v4 as uuidv4 } from "uuid";
 import img1 from "./img1.jpg";
 import img2 from "./img2.jpg";
 import img3 from "./img3.jpg";
-import img4 from "./img4.jpg";
 import img5 from "./img5.jpg";
-import img6 from "./img6.jpg";
 
 export const sliderData = [
   {
